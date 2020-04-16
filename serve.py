c2573460759037f5ab4e5b545e85fbe19bb3e7be
ak10net <35568518+ak10net@users.noreply.github.com>
2020-04-17 01:51:10 +0530

    serving result with flask

--- serve.py
+++ serve.py
@@ -1,5 +1,14 @@
-def useless_function():
-    """ 
-    A function that returns some text
-    """
-    return("some text")
+import json
+from flask import Flask, request
+from serve import get_keywords_api
+
+app = Flask(__name__)
+
+keywords_api = get_keywords_api()
+
+@app.route('/extractpackages', methods=['POST])
+def extractpackages():
+    input_data = request.json
+    output_data = keywords_api(input_data)
+    response = json.dumps(output_data)
+    return response
